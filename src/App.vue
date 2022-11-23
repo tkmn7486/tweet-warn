9de92b24e10c2a0a349c80424045b392b65adb27
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2022-11-23 20:30:36 +0900

    add

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="title">
+      <h3>DODGY TWEET</h3>
+      <small>- 怪しいTwitterアカウントのまとめサイト -</small>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="side-bar col-0 col-md-3 d-none d-md-block">
+          <router-link to="/">一覧ページ</router-link>
+          <br>
+          <router-link to="/about">このサイトについて</router-link>
+        </div>
+
+        <div class="side-bar col-2 col-md-0 d-block d-md-none">
+          <router-link to="/">
+            <img src="./assets/logo.png" class="side-icon">
+          </router-link>
+          <br>
+          <br>
+          <router-link to="/about">
+            <img src="./assets/logo.png" class="side-icon">
+          </router-link>
+        </div>
+
+        <router-view class="col col-md-9"/>
+      </div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
 <style>
@@ -15,8 +38,27 @@
   color: #2c3e50;
 }
 
+.title{
+  text-align: left;
+  border-bottom: solid 3px whitesmoke;
+  padding: 10px;
+}
+
+.side-bar{
+  border-right: solid 3px whitesmoke;
+  height: 100vh;
+  padding-top: 20px;
+  text-align: center;
+}
+
 #nav {
-  padding: 30px;
+  padding: 10px;
+  /* background-color: rgb(0, 172, 238); */
+  /* border-bottom: none; */
+}
+
+.side-bar ul li{
+  list-style: none;
 }
 
 #nav a {
@@ -26,5 +68,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.side-icon{
+  width: 10vw;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- トップページ -->
+    <div class="main-page">
+      <h3></h3>
+      <div class="list-table">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>ユーザーID</th>
+              <th>報告数</th>
+              <th></th>
+            </tr>
+            <tr v-for="item in simplify_list" :key="item.id">
+              <td>@{{item.userID}}</td>
+              <td>{{item.amount}}件</td>
+              <td>
+                <button class="btn btn-sm btn-outline-dark">詳細</button>
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+
+    <!-- 検索結果 -->
+    <div class="search-result"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import {ref,onMounted} from 'vue'
+import axios from 'axios'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup(){
+    let simplify_list = ref([])
+
+    const getList=async()=>{
+      let data = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/15kYNpG-0Pq34JV4HC8LiWI8o_yh-KZv6niWaaamQ7_Q/values/重複削除!A2:D10000?key=AIzaSyBYGo-htizvE31sI-GGUkMRsK0nZ7i5Wmw')
+
+      for(let i=0; i<data.data.values.length; i++){
+        simplify_list.value.push(
+          {
+            userID: data.data.values[i][0],
+            amount: data.data.values[i][2],
+          }
+        )
+      }
+    }
+
+    onMounted(()=>{
+      getList()
+    })
+    return{
+      simplify_list,
+      getList
+    }
   }
 }
 </script>

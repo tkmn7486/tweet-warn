6f17d60816bfba7e221d1308ab5464d51d220535
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2023-01-10 13:25:01 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="home">
     <!-- トップページ -->
-    <div class="main-page">
+    <div class="main-page" v-if="now_view == 'main'">
       <h3></h3>
       <div class="list-table">
         <table class="table">
           <thead>
             <tr>
               <th>ユーザーID</th>
-              <th>報告数</th>
+              <th>分類</th>
               <th></th>
             </tr>
             <tr v-for="item in simplify_list" :key="item.id">
               <td>@{{item.userID}}</td>
-              <td>{{item.amount}}件</td>
+              <td>{{item.type}}</td>
               <td>
-                <button class="btn btn-sm btn-outline-dark">詳細</button>
+                <button class="btn btn-sm btn-outline-dark" @click="openDetail(item)">詳細</button>
               </td>
             </tr>
           </thead>
@@ -23,32 +23,39 @@
       </div>
     </div>
 
-    <!-- 検索結果 -->
-    <div class="search-result">
-      <input type="text" v-model="search_word" @change="sortList()">
-      <div class="list-table">
+    <!-- 詳細画面 -->
+    <div class="detail-page" v-else-if="now_view == 'detail'">
+      <div class="card mt-5">
         <table class="table">
-          <thead>
+          <tbody>
             <tr>
-              <th>ユーザーID</th>
-              <th>報告数</th>
-              <th></th>
+              <td>報告日時</td>
+              <td>：</td>
+              <td>{{selected_item.time}}</td>
             </tr>
-            <tr v-if="sorted_list == []">
-              <td></td>
-              <td>報告はありません</td>
-              <td></td>
+            <tr>
+              <td>ユーザーID</td>
+              <td>：</td>
+              <td>{{selected_item.userID}}</td>
             </tr>
-            <tr v-for="item in sorted_list" :key="item.id" v-else>
-              <td>@{{item.userID}}</td>
-              <td>{{item.amount}}件</td>
-              <td>
-                <button class="btn btn-sm btn-outline-dark">詳細</button>
-              </td>
+            <tr>
+              <td>分類</td>
+              <td>：</td>
+              <td>{{selected_item.type}}</td>
             </tr>
-          </thead>
+            <tr>
+              <td>報告理由</td>
+              <td>：</td>
+              <td>{{selected_item.reason}}</td>
+            </tr>
+          </tbody>
         </table>
+        <button class="btn btn-outline-dark" @click="now_view = 'main'">閉じる</button>
       </div>
+    </div>
+
+    <div class="error-page" v-else>
+      error
     </div>
   </div>
 </template>
@@ -59,20 +66,26 @@ import axios from 'axios'
 export default {
   name: 'Home',
   setup(){
+    let now_view = ref('main')
+
     let simplify_list = ref([])
 
     let sorted_list = ref([])
 
     let search_word = ref('')
 
+    let selected_item = ref([])
+
     const getList=async()=>{
-      let data = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/15kYNpG-0Pq34JV4HC8LiWI8o_yh-KZv6niWaaamQ7_Q/values/重複削除!A2:D10000?key=AIzaSyBYGo-htizvE31sI-GGUkMRsK0nZ7i5Wmw')
+      let data = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/15kYNpG-0Pq34JV4HC8LiWI8o_yh-KZv6niWaaamQ7_Q/values/フォームの回答 1!A2:D10000?key=AIzaSyBYGo-htizvE31sI-GGUkMRsK0nZ7i5Wmw')
 
       for(let i=0; i<data.data.values.length; i++){
         simplify_list.value.push(
           {
-            userID: data.data.values[i][0],
-            amount: data.data.values[i][2],
+            time: data.data.values[i][0],
+            userID: data.data.values[i][1],
+            type: data.data.values[i][2],
+            reason: data.data.values[i][3],
           }
         )
 
@@ -86,15 +99,29 @@ export default {
       })
     }
 
+    const openDetail=(item)=>{
+      selected_item.value = []
+      selected_item.value = {
+        time: item.time,
+        userID: item.userID,
+        type: item.type,
+        reason: item.reason
+      }
+      now_view.value="detail"
+    }
+
     onMounted(()=>{
       getList()
     })
     return{
+      now_view,
       simplify_list,
       sorted_list,
       search_word,
+      selected_item,
       getList,
-      sortList
+      sortList,
+      openDetail
     }
   }
 }
